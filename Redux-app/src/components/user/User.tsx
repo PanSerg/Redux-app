03a2020eb34654dddbc37a339f8fd858ad24e694
Sergey Opanasenko <svopanasenko22@gmail.com>
2024-03-07 17:04:13 +0100

    ts finish

--- Redux-app/src/components/user/User.tsx
+++ Redux-app/src/components/user/User.tsx
@@ -2,18 +2,17 @@ import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
 export default function User() {
-    const { isLoading, error, user } = useTypedSelector(state => state.user)
+    const { isLoading, error, user } = useTypedSelector(state => state.user);
 
-    const {getUserById} = useActions()
+    const { getUserById } = useActions();
    
     return (
         <div>
-        <button onClick={() => getUserById(1)}>
-            Get user</button>
+        <button onClick={() => getUserById(1)}>Get user</button>
             {isLoading ? (
                 <div>Loading...</div>
             ) : error ? (
-                <div>{error.message}</div>
+                <div>{error}</div>
             ) : user?.name ? (
                 <h1>User: {user.name}</h1>
             ) : (
@@ -21,4 +20,4 @@ export default function User() {
             )}
         </div>
     )
-}
+};
--- Redux-app/src/store/user/user.slice.ts
+++ Redux-app/src/store/user/user.slice.ts
@@ -1,28 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { getUserById } from "./user.actions";
+import { IUser, IInitialUserState } from "../../types/user.types";
+
+const initialState: IInitialUserState = {
+  isLoading: false,
+  error: null,
+  user: {} as IUser,
+};
 
 export const userSlice = createSlice({
-    name: 'users',
-    initialState: {
-        isLoading: false,
-        error: null,
-        user: {},
-    },
-    reducers: {},
-    extraReducers: builder => {
-        builder
-          .addCase(getUserById.pending, state => {
-            state.isLoading = true;
-          })
-          .addCase(getUserById.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.user = action.payload;
-          })
-          .addCase(getUserById.rejected, (state, action) => {
-            state.isLoading = false;
-              state.error = action.payload.error
-                  state.user = {};
-          });
-    },
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getUserById.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getUserById.fulfilled, (state, action: PayloadAction<IUser>) => {
+        state.isLoading = false;
+        state.user = action.payload;
+      })
+      .addCase(getUserById.rejected, (state, action: any) => {
+        state.isLoading = false;
+        state.error = action.payload.error
+        state.user = {} as IUser;
+      });
+  },
 
-})
+});
